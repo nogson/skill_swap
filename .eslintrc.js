832da5474785da4457e6e039e920d4572b37d639
佐藤文彦 <satofaction@gmail.com>
2020-12-15 21:03:33 +0900

    修正

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    indent: [0, 4],
+    'object-curly-spacing': ["error", "never"]
+  }
+}
